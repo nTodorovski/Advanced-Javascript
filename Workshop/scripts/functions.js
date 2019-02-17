824fa68d330cfc2663cd7e918d9159596e5b2a99
Nikola Todorovski <nTodorovski@users.noreply.github.com>
2019-02-17 19:04:18 +0100

    Workshop

---
+++ Workshop/scripts/functions.js
@@ -0,0 +1,81 @@
+function createShip(ship) {
+    let ships = $("#ships1");
+    ships.append(`
+    <div class="col">
+        <div class="card" style="width: 18rem;">
+            <img src="${ship.img}" class="card-img-top" alt="${ship.name}">
+            <div class="card-header">
+                <h2>${ship.name}</h2>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Crew: ${ship.crew}</li>
+                <li class="list-group-item">Fuel: ${ship.fuel}</li>
+                <li class="list-group-item">Hull: ${ship.hull}</li>
+                <li class="list-group-item">Speed: ${ship.speed}</li>
+            </ul>
+            <div class="card-body">
+                <button id="${ship.name}" type="button" class="btn btn-primary">Select Ship</button>
+            </div>
+        </div>
+    </div>`)
+}
+
+function createPlanet(planet,ship){
+    let planets = $("#planets1");
+    planets.append(`
+    <div class="col">
+        <div class="card" style="width: 18rem;">
+            <img src="${planet.img}" class="card-img-top" alt="${planet.name}">
+            <div class="card-header">
+                <h2>${planet.name}</h2>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Size: ${planet.size}</li>
+                <li class="list-group-item">Population: ${planet.population}</li>
+                <li class="list-group-item">Distance: ${planet.distance}</li>
+                <li class="list-group-item">Development: ${planet.development}</li>
+            </ul>
+            <div class="card-body">
+                <button id="goTo${planet.name}" type="button" class="btn btn-primary">Go To</button>
+                <button id="repair${planet.name}" type="button" class="btn btn-primary">Repair</button>
+                <button id="refuel${planet.name}" type="button" class="btn btn-primary">Refuel</button>
+                <button id="hireCrew${planet.name}" type="button" class="btn btn-primary">Hire Crew Member</button>
+            </div>
+        </div>
+    </div>`)
+    $(`#goTo${planet.name}`).click(function(){
+        ship.start(planet);
+    })
+    $(`#repair${planet.name}`).click(function(){
+            planet.repair(ship);
+    })
+    $(`#refuel${planet.name}`).click(function(){
+        if(planet.shipDocked.includes(ship)){
+            planet.refuel(ship);
+        } else {
+            console.log("You must go to this planet first");
+        }
+    })
+    $(`#hireCrew${planet.name}`).click(function(){
+        if(planet.shipDocked.includes(ship)){
+            planet.hireCrewMember(ship);
+        } else {
+            console.log("You must go to this planet first");
+        }
+    })
+}
+
+function showShips() {
+    createShip(starFighter);
+    createShip(crushinator);
+    createShip(scouter);
+}
+
+function showPlanets(ship) {
+    $("#ships").remove();
+    createPlanet(rubicon9,ship);
+    createPlanet(r7,ship);
+    createPlanet(magmus,ship);
+    createPlanet(dextriaey,ship);
+    createPlanet(b18,ship);
+}
